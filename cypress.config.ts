dd6244c078d93a09055e26f0d877deeb98df6626
psimarialuisa <maria_luisasoares@hotmail.com>
2025-02-02 17:22:32 -0300

    production url

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,6 +5,6 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'https://main.dl3a1mr2fi1u7.amplifyapp.com',
   },
 })
